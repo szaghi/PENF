4642a075efe1c7e88637b64e9328716199b2d296
Stefano Zaghi <stefano.zaghi@gmail.com>
2013-03-22 12:20:28 +0100

    Introduced IR_Init for initilizing some variables that cannot be initialized into the definition statement

--- annotated.js
+++ annotated.js
@@ -1,7 +1,8 @@
 var annotated =
 [
-    [ "IR_Precision", "d6/dec/group__Library.html#d7/dc8/classIR__Precision", "d6/dec/group__Library" ],
-    [ "IR_Precision::cton", "dd/d55/group__Interface.html#d6/da2/interfaceIR__Precision_1_1cton", null ],
-    [ "IR_Precision::str", "dd/d55/group__Interface.html#d6/d7c/interfaceIR__Precision_1_1str", null ],
-    [ "IR_Precision::strz", "dd/d55/group__Interface.html#d0/d4a/interfaceIR__Precision_1_1strz", "dd/d55/group__Interface" ]
+    [ "IR_Precision", "d3/daa/group__IR__PrecisionLibrary.html#d7/dc8/classIR__Precision", "d3/daa/group__IR__PrecisionLibrary" ],
+    [ "IR_Precision::bit_size", "dd/d4b/group__IR__PrecisionInterface.html#dc/dd5/interfaceIR__Precision_1_1bit__size", null ],
+    [ "IR_Precision::cton", "dd/d4b/group__IR__PrecisionInterface.html#d6/da2/interfaceIR__Precision_1_1cton", null ],
+    [ "IR_Precision::str", "dd/d4b/group__IR__PrecisionInterface.html#d6/d7c/interfaceIR__Precision_1_1str", null ],
+    [ "IR_Precision::strz", "dd/d4b/group__IR__PrecisionInterface.html#d0/d4a/interfaceIR__Precision_1_1strz", "dd/d4b/group__IR__PrecisionInterface" ]
 ];
--- search/all_65.js
+++ search/all_65.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['endian',['endian',['../da/dd4/group__IR__Precision.html#gac8d9284e2e03650303dcb4ab5e2e17f2',1,'IR_Precision']]],
-  ['endianb',['endianB',['../da/dd4/group__IR__Precision.html#ga85040a4de331d8026e3aae3bbc3efeae',1,'IR_Precision']]],
-  ['endianl',['endianL',['../da/dd4/group__IR__Precision.html#ga2a08bfd6b7a68c08a91ce96a0fe5aed0',1,'IR_Precision']]]
+  ['endian',['endian',['../dc/d7d/group__IR__PrecisionGlobalVarPar.html#gac8d9284e2e03650303dcb4ab5e2e17f2',1,'IR_Precision']]],
+  ['endianb',['endianB',['../dc/d7d/group__IR__PrecisionGlobalVarPar.html#ga85040a4de331d8026e3aae3bbc3efeae',1,'IR_Precision']]],
+  ['endianl',['endianL',['../dc/d7d/group__IR__PrecisionGlobalVarPar.html#ga2a08bfd6b7a68c08a91ce96a0fe5aed0',1,'IR_Precision']]]
 ];
