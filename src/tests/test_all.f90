4f342a02f30c0952c4680d556f165fab003510b1
Stefano Zaghi <stefano.zaghi@gmail.com>
2016-03-25 13:01:42 +0100

    Update library name(s)
    Update library name(s): lint and sanitize the library
    
    + split *monster* module into submodules;
      + still user-friendly: access all by only 1 *use* statement;
    
    API changed: `IR_Precision` module is now `penf`: all projects using the
    old `IR_Precision` must be update.
    
    Side effects:
    
    Add the missing `bstr` for R16P kind.

--- src/tests/test_all.f90
+++ src/tests/test_all.f90
@@ -1,15 +1,15 @@
-!< Testing program for IR_Precision, Pure Fortran (2003+) library for ensuring codes portability
-program Test_Driver
+!< PENF's testing program.
+program test_all
 !-----------------------------------------------------------------------------------------------------------------------------------
-!< Testing program for IR_Precision, Pure Fortran (2003+) library for ensuring codes portability
+!< PENF's testing program.
 !<
 !<### Usage
 !<```bash
-!< ./Test_Driver
+!< ./test_all
 !<```
 !-----------------------------------------------------------------------------------------------------------------------------------
-USE IR_Precision
-USE, intrinsic:: ISO_FORTRAN_ENV, only: stdout=>OUTPUT_UNIT
+use penf
+use, intrinsic :: ISO_FORTRAN_ENV, only : stdout=>OUTPUT_UNIT
 !-----------------------------------------------------------------------------------------------------------------------------------
 
 !-----------------------------------------------------------------------------------------------------------------------------------
@@ -17,23 +17,24 @@ implicit none
 !-----------------------------------------------------------------------------------------------------------------------------------
 
 !-----------------------------------------------------------------------------------------------------------------------------------
-! initialize IR_Precision
-call IR_Init
-! print main IR_Precision values
-call IR_Print(unit=stdout)
-
+call penf_Init
+call penf_Print(unit=stdout)
 print "(A)", ''
 print "(A)", 'Testing IR_Precision tools'
 print "(A)", 'Casting real-to-string: '//str(n=1._R8P)
 print "(A)", 'Casting integer-to-string: '//str(n=11_I8P)
-print "(A,"//FR8P//")", 'Casting string-to-real: ',cton(str='2.2d0',knd=1._R8P)
-print "(A,"//FI4P//")", 'Casting integer-to-string: ',cton(str='43',knd=1_I4P)
-print "(A)", 'Casting integer-to-string with zero padding: '//trim(strz(nz_pad=3,n=34_I8P))
-! print "(A)", 'Casting real-to-bit_string: '//bstr(n=1._R4P)
+print "(A,"//FR8P//")", 'Casting string-to-real: ',cton(str='2.2d0', knd=1._R8P)
+print "(A,"//FI4P//")", 'Casting integer-to-string: ',cton(str='43', knd=1_I4P)
+print "(A)", 'Casting integer-to-string with zero padding: '//trim(strz(nz_pad=3, n=34_I8P))
+#ifndef __GFORTRAN__
+print "(A)", 'Casting real-to-bit_string: '//bstr(n=1._R4P)
+#endif
 print "(A)", 'Casting integer-to-bit_string: '//bstr(n=1_I4P)
-! print "(A,"//FR4P//")", 'Casting bit_string-to-real: ',bcton(bstr='00111111100000000000000000000000',knd=1._R4P)
-print "(A,"//FI4P//")", 'Casting bit_string-to-integer: ',bcton(bstr='00000000000000000000000000000001',knd=1_I4P)
+#ifndef __GFORTRAN__
+print "(A,"//FR4P//")", 'Casting bit_string-to-real: ', bcton(bstr='00111111100000000000000000000000', knd=1._R4P)
+#endif
+print "(A,"//FI4P//")", 'Casting bit_string-to-integer: ',bcton(bstr='00000000000000000000000000000001', knd=1_I4P)
 print "(A)", 'Number of digit of 1023: '//str(n=digit(1023_I4P))
 stop
 !-----------------------------------------------------------------------------------------------------------------------------------
-endprogram Test_Driver
+endprogram test_all
